0381924ff5582758a13cbf50a669722a2f08380e
LeanM <leandro.montellano@gmail.com>
2024-02-24 20:28:36 -0300

    CRUD api de profesiones lista

--- server/src/controllers/controller.js
+++ server/src/controllers/controller.js
@@ -35,7 +35,6 @@ const controller = {
             imagen: "owo",
             createdAt: new Date(),
             updatedAt: new Date(),
-            deletedAt: new Date(),
           });
 
           return res.status(200).json({
@@ -70,19 +69,19 @@ const controller = {
                     });
                 })
         } else {
-            const aspirantId = req.params.id;
+            const professionId = req.params.id;
             // const productImage = req.file ? req.file.filename : "producto.png"; 
-            const gettedAspirant = await db.Aspirante.findByPk(aspirantId);
+            const gettedProfession = await db.Aspirante.findByPk(professionId);
             console.log("PARAMETROS DE EDIT",req.body);
             let productImage;
             if (req.file) {
                 productImage = req.file.filename;
             } else {
-                productImage = aspirantId.image;
+                productImage = professionId.image;
             }
             const datosFormulario = req.body
             try {
-                 await gettedAspirant.update({
+                 await gettedProfession.update({
                     dni:datosFormulario.dni,
                     nombre: datosFormulario.nombre,
                     apellido: datosFormulario.apellido,
@@ -95,7 +94,7 @@ const controller = {
                     updatedAt: new Date(),
                 }, {
                     where: {
-                        id: aspirantId,
+                        id: professionId,
                     },
                 });
 
@@ -104,7 +103,7 @@ const controller = {
                         url: req.protocol + '://' + req.get('host') + req.url,
                         status: 200,
                     },
-                    data: gettedAspirant,
+                    data: gettedProfession,
                 })
             } catch (error) {
                 return res.status(403).json({
@@ -173,6 +172,102 @@ const controller = {
             },
             data: profession
         })
+    },
+    addProfession: async (req, res) => {
+        const datosFormulario = req.body
+        console.log("DATOS SON",datosFormulario)
+        const ProfesionNueva = await db.Profesiones.create({
+            nombre: datosFormulario.nombre,
+            createdAt:  new Date(),
+            updatedAt: new Date(),
+          });
+
+          return res.status(200).json({
+            meta: {
+                url: req.protocol + '://' + req.get('host') + req.url,
+                status: 200,
+            },
+            data: ProfesionNueva,
+        })
+      },
+      updateProfession: async (req, res) => {
+        // const resultValidation = validationResult(req);
+        const resultValidation = {
+            errors:0,
+        };
+        console.log("PARAMETROS DE PARAMS",req.params.id);
+
+        if (resultValidation.errors.length > 0) {
+            const id = req.params.id;
+            db.Profesiones.findByPk(id, { raw: true })
+                .then((result) => {
+                    const mergedData = {
+                        ...result,
+                        ...req.body,
+                    };
+                    console.log("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRR*************************")
+                    res.render(path.join(__dirname, "../views/admin/editProduct.ejs"), {
+                        errors: resultValidation.mapped(),
+                        oldData: req.body,
+                        productToEdit: mergedData,
+
+                    });
+                })
+        } else {
+            const professionId = req.params.id;
+            // const productImage = req.file ? req.file.filename : "producto.png"; 
+            const gettedProfession = await db.Profesiones.findByPk(professionId);
+            console.log("PARAMETROS DE EDIT",req.body);
+            const datosFormulario = req.body
+            try {
+                 await gettedProfession.update({
+                    nombre: datosFormulario.nombre,
+                    updatedAt: new Date(),
+                }, {
+                    where: {
+                        id: professionId,
+                    },
+                });
+
+                return res.status(200).json({
+                    meta: {
+                        url: req.protocol + '://' + req.get('host') + req.url,
+                        status: 200,
+                    },
+                    data: gettedProfession,
+                })
+            } catch (error) {
+                return res.status(403).json({
+                    meta: {
+                        url: req.protocol + '://' + req.get('host') + req.url,
+                        status: 403,
+                    }
+                })
+            }
+        }
+    },
+    removeProfession: async (req, res) => {
+        try {
+            db.Profesiones.destroy({
+                where: {
+                    id: req.params.id,
+                },
+            });           
+           return res.status(200).json({
+               meta: {
+                   url: req.protocol + '://' + req.get('host') + req.url,
+                   status: 200,
+               },
+               data: "Profesión removida exitosamente.",
+           })
+       } catch (error) {
+           return res.status(403).json({
+               meta: {
+                   url: req.protocol + '://' + req.get('host') + req.url,
+                   status: 403,
+               }
+           })
+       }
     },
     professions: async (req, res) => {
         const professions = await db.Profesiones.findAll(
--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -3,17 +3,21 @@ const router = express.Router();
 
 const controller = require('../controllers/controller');
 
-router.get('/applicants', controller.applicants);
+router.get('/applicants', controller.applicants); // List all
 
-router.get('/applicant/:id', controller.applicant);
+router.get('/applicant/:id', controller.applicant); // Read one
 
+// ASPIRANTES
 router.post('/applicant/', controller.addApplicant); // Create
 router.post('/updateApplicant/:id', controller.updateApplicant); // Update
 router.post('/deleteApplicant/:id', controller.removeApplicant); // Delete
 // router.post('/updateApplicant/:id', controller.applicant);
 
-
-router.get('/professions', controller.professions);
+// PROFESIONES
 router.get('/profession/:id', controller.profession);
+router.get('/professions', controller.professions);
+router.post('/addprofession/', controller.addProfession); // Create
+router.post('/updateprofession/:id', controller.updateProfession); // Update
+router.post('/deleteprofession/:id', controller.removeProfession); // Delete
 
 module.exports=router
