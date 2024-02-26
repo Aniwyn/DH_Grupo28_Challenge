a89a239eabdbb47012c2bd2bfbc3c86d5ab67f2d
LeanM <leandro.montellano@gmail.com>
2024-02-25 21:49:53 -0300

    Primera adaptacion de form

--- client/src/pages/Inscribe.jsx
+++ client/src/pages/Inscribe.jsx
@@ -3,46 +3,50 @@ function Inscribe() {
     <div className="px-8 pt-5">
       <h1 className="font-bold text-blue-900 text-lg">Añade un aspirante aquí...</h1>
       {/* <div className="w-100 h-[1px] bg-gray-300 my-4"></div> */}
-      <form action="" className="flex flex-col justify-s w-full space-y-2 font- text-sm text-slate-600">
+      <form action="/api/applicant" method="post" enctype="multipart/form-data" className="flex flex-col justify-s w-full space-y-2 font- text-sm text-slate-600">
         <h2 className="mt-2 font-medium text-slate-600 text-md">Datos personales</h2>
         <div className="w-100 h-[0.5px] bg-gray-300 my-2 "></div>
         <div className="flex flex-row w-full">
           <label htmlFor="dni" className="w-1/5">D.N.I.</label>
-          <input type="text" placeholder="Ingresa un DNI..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
+          <input id="dni" type="text" placeholder="Ingresa un DNI..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
         </div>
         <div className="flex flex-row w-full">
           <label htmlFor="lastname" className="w-1/5">Apellido</label>
-          <input type="text" placeholder="Ingresa tu apellido..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
+          <input id="last_name" type="text" placeholder="Ingresa tu apellido..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
         </div>
         <div className="flex flex-row w-full">
           <label htmlFor="name" className="w-1/5">Nombre/s</label>
-          <input type="text" placeholder="Ingresa tu nombre..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
+          <input id="name" type="text" placeholder="Ingresa tu nombre..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
+        </div>
+        <div className="flex flex-row w-full">
+          <label htmlFor="name" className="w-1/5">Teléfono</label>
+          <input id="phone" type="text" placeholder="Ingresa tu número de teléfono..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
         </div>
         <div className="flex flex-row w-full">
           <label htmlFor="gender" className="w-1/5">Género</label>
-          <select id="alternativas" className="w-1/2 border px-1 rounded-sm px-2 py-1 placeholder:ml-5 hover:border-gray-400" name="alternativas">
-            <option value="masculino">Masculino</option>
-            <option value="femenino">Femenino</option>
-            <option value="otro">Otro</option>
+          <select id="gender_id" className="w-1/2 border px-1 rounded-sm px-2 py-1 placeholder:ml-5 hover:border-gray-400" name="alternativas">
+            <option value="1">Masculino</option>
+            <option value="2">Femenino</option>
+            <option value="3">Otro</option>
           </select><br></br>
         </div>
         <div className="flex flex-row w-full">
           <label htmlFor="fecha" className="w-1/5">Fecha de nacimiento</label>
-          <input type="date" id="fecha" name="fecha" className="w-1/2 border px-1 rounded-sm px-2 py-1 placeholder:ml-5 hover:border-gray-400" />
+          <input id="birth_date" type="date" name="fecha" className="w-1/2 border px-1 rounded-sm px-2 py-1 placeholder:ml-5 hover:border-gray-400" />
         </div>
         <div className="flex flex-row w-full">
           <label htmlFor="email" className="w-1/5">Correo electrónico</label>
-          <input type="text" placeholder="Ingresa tu email..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
+          <input id="email" type="text" placeholder="Ingresa tu email..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
         </div>
         <h2 className="pt-4 font-medium text-slate-600 text-md">Datos profesionales</h2>
         <div className="w-100 h-[0.5px] bg-gray-300 my-2 "></div>
         <div className="flex flex-row w-full">
           <label htmlFor="linkedin" className="w-1/5">Perfil de LinkedIn</label>
-          <input type="text" placeholder="Ingresa tu perfil de LinkedIn..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
+          <input id="url_linkedin" type="text" placeholder="Ingresa tu perfil de LinkedIn..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
         </div>
         <div className="flex flex-row w-full">
           <label htmlFor="imagen" className="w-1/5">Imagen</label>
-          <input type="file" placeholder="Ingresa tu fotografía..." className="w-1/2 pl-2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400 
+          <input id="image" type="file" placeholder="Ingresa tu fotografía..." className="w-1/2 pl-2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400 
           file:mr-4 file:py-1 file:px-2
           file:border-0
           file:text-xs file:font-md
