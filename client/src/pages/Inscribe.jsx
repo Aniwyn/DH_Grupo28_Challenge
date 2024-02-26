2ad2d0edcec6aa2aea13e4ca5ceb783134ba863a
LeanM <leandro.montellano@gmail.com>
2024-02-25 21:55:05 -0300

    Peq cambios

--- client/src/pages/Inscribe.jsx
+++ client/src/pages/Inscribe.jsx
@@ -11,7 +11,7 @@ function Inscribe() {
           <input id="dni" type="text" placeholder="Ingresa un DNI..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
         </div>
         <div className="flex flex-row w-full">
-          <label htmlFor="lastname" className="w-1/5">Apellido</label>
+          <label htmlFor="last_name" className="w-1/5">Apellido</label>
           <input id="last_name" type="text" placeholder="Ingresa tu apellido..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
         </div>
         <div className="flex flex-row w-full">
@@ -19,20 +19,20 @@ function Inscribe() {
           <input id="name" type="text" placeholder="Ingresa tu nombre..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
         </div>
         <div className="flex flex-row w-full">
-          <label htmlFor="name" className="w-1/5">Teléfono</label>
+          <label htmlFor="phone" className="w-1/5">Teléfono</label>
           <input id="phone" type="text" placeholder="Ingresa tu número de teléfono..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
         </div>
         <div className="flex flex-row w-full">
-          <label htmlFor="gender" className="w-1/5">Género</label>
+          <label htmlFor="gender_id" className="w-1/5">Género</label>
           <select id="gender_id" className="w-1/2 border px-1 rounded-sm px-2 py-1 placeholder:ml-5 hover:border-gray-400" name="alternativas">
-            <option value="1">Masculino</option>
+            <option value="1">Masculinno</option>
             <option value="2">Femenino</option>
             <option value="3">Otro</option>
           </select><br></br>
         </div>
         <div className="flex flex-row w-full">
-          <label htmlFor="fecha" className="w-1/5">Fecha de nacimiento</label>
-          <input id="birth_date" type="date" name="fecha" className="w-1/2 border px-1 rounded-sm px-2 py-1 placeholder:ml-5 hover:border-gray-400" />
+          <label htmlFor="birth_date" className="w-1/5">Fecha de nacimiento</label>
+          <input id="birth_date" type="date" name="birth_date" className="w-1/2 border px-1 rounded-sm px-2 py-1 placeholder:ml-5 hover:border-gray-400" />
         </div>
         <div className="flex flex-row w-full">
           <label htmlFor="email" className="w-1/5">Correo electrónico</label>
@@ -41,11 +41,11 @@ function Inscribe() {
         <h2 className="pt-4 font-medium text-slate-600 text-md">Datos profesionales</h2>
         <div className="w-100 h-[0.5px] bg-gray-300 my-2 "></div>
         <div className="flex flex-row w-full">
-          <label htmlFor="linkedin" className="w-1/5">Perfil de LinkedIn</label>
+          <label htmlFor="url_linkedin" className="w-1/5">Perfil de LinkedIn</label>
           <input id="url_linkedin" type="text" placeholder="Ingresa tu perfil de LinkedIn..." className="w-1/2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400" />
         </div>
         <div className="flex flex-row w-full">
-          <label htmlFor="imagen" className="w-1/5">Imagen</label>
+          <label htmlFor="image" className="w-1/5">Imagen</label>
           <input id="image" type="file" placeholder="Ingresa tu fotografía..." className="w-1/2 pl-2 border px-1 rounded-sm py-1 placeholder:ml-5 hover:border-gray-400 
           file:mr-4 file:py-1 file:px-2
           file:border-0
